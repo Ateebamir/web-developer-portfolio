aa417ca7f253e739a5c9221b554e4042abb64fc5
Ateebamir <malik26.tbox@gmail.com>
2025-09-09 16:14:58 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Browser ko bola ke apna default scroll restore na kare
+    window.history.scrollRestoration = "manual";
+    // Har reload pe top se start karna
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Toaster />
@@ -13,8 +21,6 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
- 
- 
     </>
   );
 }
